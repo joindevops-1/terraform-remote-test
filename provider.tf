443ab4487c056bd61d715745bcfafe2376e88594
sivakumarreddym <info@joindevops.com>
2025-02-20 20:35:00 +0530

    S3 PART-02

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "82s-tf-remote-state"
+    bucket = "remote-test-secure"
     key    = "expense-backend-infra" # you should have unique keys with in the bucket, same key should not be used in other repos or tf projects
     region = "us-east-1"
     dynamodb_table = "82s-state-locking"
